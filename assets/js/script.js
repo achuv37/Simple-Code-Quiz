72352f811813207ba8a2c43dd0a92b3f9db52b65
Achu Ajesh <achuv37@gmail.com>
2022-03-04 08:28:51 -0500

    Add pseudocode

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// Pseudo code
+// 1. Set a timer to a button with a starting value of 0.
+// 2. Set a countdown for starting the Quiz.
+// 3. When the user clicks on Start Quiz button, then questions will appear with choices.
+// 4. A text displays "correct" when the user clicks the right answer.
+// 5. A text displays "wrong" when the user clicks the wrong answer.
+// 6. Display final score based on user's right answers.
+// 7. Show the user leftover time, which is subtracted from final score.
+// 8. Display final scores with initials.
+// 9. set local storage.
+// 10. Display High score.
